bd9fedc946468406e0d4e706516b0a17cb7d1ad4
{hwayoon] <{rup_rupes@naver.com}>
2024-06-30 20:49:42 +0900

    Course for Styling in React projects done

--- 03-module-introduction-challenge/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header({ children }) {
-  return (
-    <header id="header">
-      <img src="src\assets\investment-calculator-logo.png" />
-      {children}
-    </header>
-  );
-}
--- 03-module-introduction-challenge/src/Input.jsx
+++ 03-module-introduction-challenge/src/Input.jsx
@@ -1,3 +1,38 @@
+import { calculateInvestmentResults, formatter } from "./util/investment";
+import { useState } from "react";
+
 export default function Input() {
-  return <input id="user-input" />;
+  const [inputs, setInputs] = useState();
+
+  function handleInputChange() {
+    inputs = [document.querySelectorAll(".input-group")];
+    calculateInvestmentResults({ ...inputs.event.target.value });
+  }
+
+  const inputText = [
+    "INITIAL INVESTMENT",
+    "ANNUAL INVESTMENT",
+    "EXPECTED RETURN",
+    "DURATION",
+  ];
+  return (
+    <div id="user-input">
+      <label className="input-group">
+        {inputText[0]}
+        <input />
+      </label>
+      <label className="input-group">
+        {inputText[1]}
+        <input />
+      </label>
+      <label className="input-group">
+        {inputText[2]}
+        <input />
+      </label>
+      <label className="input-group">
+        {inputText[3]}
+        <input />
+      </label>
+    </div>
+  );
 }
